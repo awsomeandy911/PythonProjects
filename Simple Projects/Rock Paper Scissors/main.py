3fd61adce821be21e4222b9f0bc206063647862a
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-09-10 01:49:09 -0500

    Updated to include uppercase

--- Simple Projects/Rock Paper Scissors/main.py
+++ Simple Projects/Rock Paper Scissors/main.py
@@ -4,21 +4,21 @@ import random
 def play():
     print("Welcome to the game of Rock, Paper, Scissors!!!")
     # have user enter rock, paper, or scissors
-    user = input("'R' for rock, 'P' for paper, 'S' for scissors"). lower() # keeps user input lowercase
+    user = input("'R' for rock, 'P' for paper, 'S' for scissors").upper() # keeps user input uppercase
     # computer randomly chooses a move
-    computer = random.choice(['r', 'p', 's'])
+    computer = random.choice(['R', 'P', 'S'])
     if user == computer:
-        return f'It\'s a tie! Computer played {computer}'
+        return f'It\'s a tie! Computer played: {computer}'
     if isWin(user, computer):
-        return 'You Won!!!'
+        return f'You Won!!! Computer played: {computer} :)'
     
-    return f'You lost! Computer played {computer} :('
+    return f'You lost! Computer played: {computer} :('
 
 # rock > scissors, scissors > paper, paper > rock
 def isWin(player, opponent):
     # returns true if player beats opponent 
-    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
-        or (player == 'p' and opponent == 'r'):
+    if (player == 'R' and opponent == 'S') or (player == 'S' and opponent == 'P') \
+        or (player == 'P' and opponent == 'R'):
         return True
     
 print(play())
