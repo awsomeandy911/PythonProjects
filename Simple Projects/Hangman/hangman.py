41b55da992b885ccf920ee8691967343619223a8
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-09-11 22:53:50 -0500

    Updated hangman function

--- Simple Projects/Hangman/hangman.py
+++ Simple Projects/Hangman/hangman.py
@@ -1,12 +1,21 @@
 # import random library package and words from words.py
 import random
 from words import words
+import string
 
-def getWord(words):
-    word = random.choice(words) #randomly chooses something from the list
+# function to get valid words
+def getValidWord(words):
+    # randomly chooses something from the list
+    word = random.choice(words) 
     while '-' in word or ' ' in word:
         word = random.choice(words)
-    return word
+    return word.upper() # keeps user input uppercase
 
+# function to keep track of valid words
 def hangman():
-    word = getWord(words)
+    word = getValidWord(words)
+    # letters in the word
+    wordLetters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    # what user has guessed
+    usedLetters = set()
