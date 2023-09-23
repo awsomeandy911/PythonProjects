ba52f40a5707cdc5f31d1f18930347355517b355
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-09-23 15:49:40 -0500

    Finished most of hangman need to add lives still

--- Simple Projects/Hangman/hangman.py
+++ Simple Projects/Hangman/hangman.py
@@ -20,16 +20,25 @@ def hangman():
     # what letters user has guessed
     guessedLetters = set()
     # get user input
-    userLetter = input("Guess a Letter: ").upper() # keeps input upper case
-    if userLetter in alphabet - guessedLetters:
-        guessedLetters.add(userLetter)
-        if userLetter in wordLetters:
-            wordLetters.remove(userLetter)
-    
-    elif userLetter in guessedLetters:
-        print("You literally just guessed that letter -_- Try again")
-    
-    else:
-        print("Invalid character, Try again")
+    while len(wordLetters) > 0:
+        # letters used
+        print("You have used these letters: ", " ".join(guessedLetters)) #" ".join(['a', 'b', 'c']) --> "a b c"
+
+        # current status of the word i.e (W _ R D)
+        worldList = [letter if letter in guessedLetters else '_' for letter in word]
+        print("Current word: ", " ".join(worldList))
+
+        userLetter = input("Guess a Letter: ").upper() # keeps input upper case
+        if userLetter in alphabet - guessedLetters:
+            guessedLetters.add(userLetter)
+            if userLetter in wordLetters:
+                wordLetters.remove(userLetter)
+        
+        elif userLetter in guessedLetters:
+            print("You literally just guessed that letter -_- Try again")
+        
+        else:
+            print("Invalid character, Try again")
+    print("Yay you guessed the word!: " + word)
 
 hangman()
