79c7155b39b8261e3dd7c4fdc17f5949a75abea3
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-09-25 01:29:11 -0500

    Add lives to hangman and clean up code

--- Simple Projects/Hangman/hangman.py
+++ Simple Projects/Hangman/hangman.py
@@ -19,10 +19,14 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     # what letters user has guessed
     guessedLetters = set()
-    # get user input
-    while len(wordLetters) > 0:
+
+    # tracks amount of lives user has
+    lives = 6
+
+    # get user input with these conditions
+    while len(wordLetters) > 0 and lives > 0:
         # letters used
-        print("You have used these letters: ", " ".join(guessedLetters)) #" ".join(['a', 'b', 'c']) --> "a b c"
+        print("You have used these letters:", " ".join(guessedLetters),"and you have", lives, "lives left") #" ".join(['a', 'b', 'c']) --> "a b c"
 
         # current status of the word i.e (W _ R D)
         worldList = [letter if letter in guessedLetters else '_' for letter in word]
@@ -33,12 +37,20 @@ def hangman():
             guessedLetters.add(userLetter)
             if userLetter in wordLetters:
                 wordLetters.remove(userLetter)
+            else:
+                lives = lives - 1 # takes away 1 life if user guessed wrong
+                print("The letter " + userLetter + " is not in the word")
         
         elif userLetter in guessedLetters:
             print("You literally just guessed that letter -_- Try again")
         
         else:
             print("Invalid character, Try again")
-    print("Yay you guessed the word!: " + word)
+    
+    if lives == 0:
+        print("GG, you lost all your lives. Try Again")
+    else:
+        print("Yay you guessed the word!: " + word)
+    
 
 hangman()
