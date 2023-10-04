38c0c7a5b2f1e2f15a352144f2f181bc910788fb
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-10-04 01:50:20 -0500

    Updated Player class and add new game file

---
+++ Simple Projects/Tic Tac Toe/game.py
@@ -0,0 +1,3 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)] # use single list to represnt 3x3 tic tac toe board
--- Simple Projects/Tic Tac Toe/player.py
+++ Simple Projects/Tic Tac Toe/player.py
@@ -11,3 +11,20 @@ class Player:
     # players need to get their next move given a game
     def getMove(self, game):
         pass
+
+# Computer player class built from inheritance from Player
+class RandomComputerPlayer(Player):
+    # initialize super class
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def getMove(self, game):
+        pass
+
+# Human player class built from inheritance from Player
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def getMove(self, game):
+        pass
