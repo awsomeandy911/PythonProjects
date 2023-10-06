523c717ebe5146465353644eeace3be1cec3307e
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-10-06 03:40:37 -0500

    add method to print tic tac toe board

--- Simple Projects/Tic Tac Toe/game.py
+++ Simple Projects/Tic Tac Toe/game.py
@@ -1,3 +1,9 @@
 class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)] # use single list to represnt 3x3 tic tac toe board
+        self.currentWinner = None # keeps track of winner
+
+    def printBoard(self):
+        # to get rows for game
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print("| " + " | ".join(row) + " |")
