7af98ea912601d4f8366ba15a28b6ff58ef0e6d9
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-10-14 20:43:17 -0500

    Update game.py file

--- Simple Projects/Tic Tac Toe/game.py
+++ Simple Projects/Tic Tac Toe/game.py
@@ -7,3 +7,19 @@ class TicTacToe:
         # to get rows for game
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
             print("| " + " | ".join(row) + " |")
+
+    @staticmethod
+    def printBoardNumbers():
+        # 0 | 1 | 2 etc (tells us what number corresponds to what box)
+        numberBoard = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in numberBoard:
+            print("| " + " | ".join(row) + " |") 
+
+    def availableMoves(self):
+        # return []
+        moves = []
+        for (i, spot) in enumerate(self.board):
+            # ['x', 'x', 'o'] the spot of X's and O's
+            if spot == ' ':
+                moves.append(i)
+        return moves
